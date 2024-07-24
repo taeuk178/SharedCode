4766cf986b38802b633d69ce3de12597b9c875e4
taeuk178 <nous178@naver.com>
2024-07-24 21:54:27 +0900

    Test: code Test

--- SharedCode.swift
+++ SharedCode.swift
@@ -1,1 +1,12 @@
-print('Hello')
+
+public struct SharedCode {
+    public static func Test() {
+        print("hello subTree")
+    }
+}
+
+struct CodeTest {
+    static func Test() {
+        
+    }
+}
